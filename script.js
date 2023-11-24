b1892f019523ae7bf6da2f1c4a2b627183be533a
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2023-11-24 00:27:05 -0300

    Creating js

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const apiKey = '20345eec75350cb0d4cfc2c57a77d2a9';
+
+    function obterLocalizacao() {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+    }
+
+    function obterDadosMeteorologicos(latitude, longitude) {
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&lang=pt`;
+
+        return fetch(apiUrl)
+        .then(response => response.json())
+        .catch(error => console.error('Erro na requisição:', error));
+    }
+
+    function exibirDadosMeteorologicos(dados) {
+        const cidadeElemento = document.getElementById('cidade');
+        const temperaturaElemento = document.getElementById('temperatura');
+        const condicaoElemento = document.getElementById('condicao');
+
+        cidadeElemento.textContent = `Cidade: ${dados.name}`;
+
+        const temperaturaCelsius = Math.round(dados.main.temp - 273.15);
+
+        const condicao = dados.weather[0].description;
+        const condicaoComPrimeiraLetraMaiuscula = condicao.charAt(0).toUpperCase() + condicao.slice(1);
+
+        temperaturaElemento.textContent = `Temperatura: ${temperaturaCelsius}°C`;
+        condicaoElemento.textContent = `Condição: ${condicaoComPrimeiraLetraMaiuscula}`;
+    }
+
+    obterLocalizacao() 
+    .then(posicao => obterDadosMeteorologicos(posicao.coords.latitude, posicao.coords.longitude))
+    .then(dados => exibirDadosMeteorologicos(dados))
+    .catch(error => console.error('Erro', error))
+});
